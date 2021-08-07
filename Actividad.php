386164472f4b30b9065ed84b8a531d51a156890d
Andrea Scalezi <scaleziandrea@gmail.com>
2021-08-07 18:31:19 -0300

    Subiendo actividad

---
+++ Actividad.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Actividad - Unidad 2</title>
+</head>
+<body>
+    <?php
+    $nombre = "Andrea";
+    $apellido = "Scalezi";
+    $edad = "32";
+    $hobbie = "cocinar";
+    $editor = "VSC";
+    $sistemaOperativo = "Linux";
+
+    echo "<p>"."Mi nombre es ".$nombre."</p>";
+    echo "<p>"."Mi apellido es ".$apellido."</p>";
+    echo "<p>"."Tengo ".$edad." años"."</p>";
+    echo "<p>"."Mi hobbie es ".$hobbie."</p>";
+    echo "<p>"."Mi editor de código favorito es ".$editor."</p>";
+    echo "<p>"."El sistema operativo que uso es ".$sistemaOperativo."</p>";    
+    ?>
+</body>
+</html>
